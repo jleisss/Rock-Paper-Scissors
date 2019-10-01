31acd5b48664c6ffbc26ed8d5197cdac366bc2d3
jleisss <56050412+jleisss@users.noreply.github.com>
2019-10-01 14:51:15 -0700

    practice2

---
+++ practice2.py
@@ -0,0 +1,12 @@
+number1 = int(input("Please enter a number, this will be the first number represented in seperate equations using the four basic mathematical functions (+, -, *, /)  "))
+number2 = int(input("Now enter another number, this will be the second number represented in seperate equations using the four basic mathematical funtions (+, -, *, /) "))
+
+print("Added together, the two numbers equal " + str(number1 + number2))
+print("The first number subtracted from the second equals " + str(number1 - number2))
+print("Multiplied together, the two numbers equal " + str(number1 * number2))
+print("The first number divided by the second number equals " + str(number1 / number2))
+
+
+
+
+
