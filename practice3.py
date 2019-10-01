d59354f9d38be4c0657ff1c01ffce6ddcd1b2b26
jleisss <56050412+jleisss@users.noreply.github.com>
2019-10-01 14:51:43 -0700

    practice3

---
+++ practice3.py
@@ -0,0 +1,10 @@
+day= input("What day is it? ")
+
+if day == "Monday":
+	print("It's Monday, the weekend is over")
+elif day == "Friday":
+	print("It's Friday, the weekend is close")
+elif day == "Saturday":
+	print("It's the weekend, time to relax")
+else:
+	print("You must enter a day of the week, or there might be an error in your formating")
