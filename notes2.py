ae3bed761cffc3d407aa3defe7ec04561dbd8cbc
jleisss <56050412+jleisss@users.noreply.github.com>
2019-10-01 14:48:23 -0700

    notes

---
+++ notes2.py
@@ -0,0 +1,24 @@
+from turtle import *
+# This is a commment, use them a lot yeet
+
+# Math operators, there are 6
+# +, -, *, /, ** (exponents), % (modulo division)
+print(4 + 8) # 12
+print(8 - 2) # 6
+print(-3 * -2) # 6
+print(6 / 4) # 1.5
+print(2 ** 4) # 16
+print(12 % 5) # 2
+print(6 % 3) # 0
+print(16 % 9) # 7
+
+# + , and * are overloaded that means they do different things based on
+# imputs
+
+print("Hello" + "There")
+print("4" + "8")
+print("4" + str(8)) # casting
+# example of a prompt
+name = input("What is your name?: ")
+print("Hello " + name)
+
