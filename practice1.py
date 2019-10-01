dd33e700dfbfd33918ef362c2fd041bb2d553173
jleisss <56050412+jleisss@users.noreply.github.com>
2019-10-01 14:49:55 -0700

    practice1

---
+++ practice1.py
@@ -0,0 +1,23 @@
+from turtle import *
+# J.P. Leis
+# Sept 23, 2019
+# Period 6
+# practice1.py
+print("4 * 8 = " + str(4 * 8))
+print("12 / 2 = " + str(12 / 2))
+print("-5 - 6 * 3 = " + str(-5 - 6 * 3))
+# prompt for the user's age
+age = input("What is your age? ")
+# start here 
+age = int(age)+10
+print("In ten years you will be " + str(age))
+print("Go Vipers!\n" * 20)
+print("H\ne\nl\nl\no")
+myNum = 20
+myNum = myNum+5
+myOtherNum = 10
+myNum = myNum+myOtherNum
+print("My num is " + str(myNum))
+
+
+
